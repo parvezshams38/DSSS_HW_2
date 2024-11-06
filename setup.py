4cd2c55cfb259b02d956ccc16502ae6207f4b73d
parvezshams38 <parvezshams38@gmail.com>
2024-11-06 17:01:13 +0100

    still trying to pip installable

--- setup.py
+++ setup.py
@@ -1,20 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="math_quiz",
-    version="1.0",
-    packages=find_packages(),
-    entry_points={
-        "console_scripts": [
-            "math_quiz=math_quiz.math_quiz:math_quiz"
-        ]
-    },
-    author="Md Salahuddin Parvez",
-    url="https://github.com/parvezshams38/DSSS_HW_2",
+    name='math_quiz',
+    version='0.1.0',    
+    description='A example Python package',
+    url='https://github.com/shuds13/math_quiz',
+    author='Md Salahuddin Parvez',
+    license='BSD 2-clause',
+    packages=['pyexample'],
+    install_requires=['mpi4py>=2.0',
+                      'numpy',                     
+                      ],
+
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
-    python_requires='>=3.6',
 )
